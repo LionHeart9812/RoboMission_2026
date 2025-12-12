1c4bf24f1f6fa6b4f7803391c225a97ba57f4e97
LionHeart <leolev.nabok@gmail.com>
2025-12-12 14:31:50 +0100

    RoboSuess 1.1.1

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 from pixycamev3.pixy2 import Pixy2
 import time
+from linefollower import DriveTillDouble, DriveTillColor, LineFollower_tillDouble
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
@@ -50,137 +51,6 @@ def bremsen():
     motor_left.stop()
     motor_right.stop()
 
-
-# Fahren bis zur einer doppelten schwarzen Linie
-def DriveTillDouble(colorIndex, speed):
-    colorReflection_left =  colorSensor_left.reflection()
-    colorReflection_right = colorSensor_right.reflection()
-    seenBlack = 0
-
-    while seenBlack == 0: 
-        colorReflection_left =  colorSensor_left.reflection()
-        colorReflection_right = colorSensor_right.reflection()
-        # Einfach gerade aus fahren mit bestimmter Geschwindigkeit (speed)
-        fahren(speed)
-        # Wenn beide Farbsensoren eine bestimmte Farbe (colorIndex) sehen, dann stoppen
-        if colorReflection_left < colorIndex + 3 and colorReflection_left > colorIndex - 3 and colorReflection_right < colorIndex + 3 and colorReflection_right > colorIndex - 3:
-            bremsen()
-            ev3.speaker.beep()
-            print("color sensor left: " + str(colorReflection_left))
-            print("color sensor right: " + str(colorReflection_right))
-            print("----------------------------")
-            seenBlack = 1
-
-def DriveTillColor(whichSensor, colorIndex, speed):
-    if whichSensor == "left":
-        colorReflection_left =  colorSensor_left.reflection()
-        seenBlack = 0
-
-        while seenBlack == 0: 
-            colorReflection_left =  colorSensor_left.reflection()
-            # Einfach gerade aus fahren mit variable speed
-            fahren(speed)
-            # Wenn ein ausgewählter (whichsensor) Farbsenor eine bestimmte Farbe (colorIndex) sieht, dann stoppen
-            if colorReflection_left < colorIndex + 3 and colorReflection_left > colorIndex - 3:
-                bremsen()
-                ev3.speaker.beep()
-                print("color sensor left: " + str(colorReflection_left))
-                print("----------------------------")
-                seenBlack = 1
-
-    elif whichSensor == "right":
-        colorReflection_right =  colorSensor_right.reflection()
-        seenBlack = 0
-
-        while seenBlack == 0: 
-            colorReflection_right =  colorSensor_right.reflection()
-            # Einfach gerade aus fahren mit variable speed
-            fahren(speed)
-            # Wenn ein ausgewählter (whichsensor) Farbsenor eine bestimmte Farbe (colorIndex) sieht, dann stoppen
-            if colorReflection_right < colorIndex + 3 and colorReflection_right > colorIndex - 3:
-                bremsen()
-                ev3.speaker.beep()
-                print("color sensor left: " + str(colorReflection_right))
-                print("----------------------------")
-                seenBlack = 1
-
-
-#Linienfolger mit Bedingung --> Bis zu einer doppelten schwarzen Linie
-def LineFollower_tillDouble():
-    colorReflection_left =  colorSensor_left.reflection()
-    colorReflection_right = colorSensor_right.reflection()
-    seenBlack = 0
-    seeingNothing = False
-
-    while seenBlack == 0:
-        colorReflection_left =  colorSensor_left.reflection()
-        colorReflection_right = colorSensor_right.reflection()
-
-        if colorReflection_left < 3 and colorReflection_right < 3 and seeingNothing == False:
-            seeingNothing = True
-
-        # Wenn linker Farbsensor auf schwarz, fahre etwas nach rechts
-        elif colorReflection_left < 15 and seeingNothing == False:
-            print("Links")
-            robot.stop()
-            while colorReflection_left < 15 and seenBlack == 0:
-                colorReflection_left =  colorSensor_left.reflection()
-                colorReflection_right = colorSensor_right.reflection()
-
-                robot.drive(100, -40)
-                seenBlack = 0
-
-                if colorReflection_left < 3 and colorReflection_right < 3:
-                    seeingNothing = True
-
-                # Wenn beide schwarz stoppen
-                if colorReflection_right < 15 and colorReflection_left < 15 and seeingNothing == False:
-                    seenBlack = 1
-                    robot.stop()
-                    bremsen()
-                    ev3.speaker.beep()
-                    print("color sensor left: " + str(colorReflection_left))
-                    print("color sensor right: " + str(colorReflection_right))
-                    print("----------------------------")
-
-        # Wenn rechter, dann etwas nach links
-        elif colorReflection_right < 15 and seeingNothing == False:
-            print("Rechts")
-            robot.stop()
-            while colorReflection_right < 15 and seenBlack == 0:
-                colorReflection_left =  colorSensor_left.reflection()
-                colorReflection_right = colorSensor_right.reflection()
-
-                robot.drive(100, 40)
-                seenBlack = 0
-
-                if colorReflection_left < 3 and colorReflection_right < 3:
-                    seeingNothing = True
-
-                # Wenn beide schwarz stoppen
-                if colorReflection_right < 15 and colorReflection_left < 15 and seeingNothing == False:
-                    seenBlack = 1
-                    robot.stop()
-                    bremsen()
-                    ev3.speaker.beep()
-                    print("color sensor left: " + str(colorReflection_left))
-                    print("color sensor right: " + str(colorReflection_right))
-                    print("----------------------------")
-
-        # Wenn kein Sensor auf schwarz, oder nichts erkennen, fahre geradeaus
-        elif colorReflection_right > 14 and colorReflection_left > 14 or seeingNothing == True:
-            robot.stop()
-            print("Geradeaus")
-            while colorReflection_right > 14 and colorReflection_left > 14 or seeingNothing == True:
-                colorReflection_left =  colorSensor_left.reflection()
-                colorReflection_right = colorSensor_right.reflection()
-
-                robot.drive(300, 0)
-                seenBlack = 0
-
-                if colorReflection_left > 2 or colorReflection_right > 2:
-                    seeingNothing = False
-
 #Erkennen der Süßigkeitenkiste
 def KisteErkennen():
     minimal_middle = 147.5
@@ -218,12 +88,13 @@ def BonbonErkennen():
     start = time.time()
 
     while time.time() - start < 2.75:
-        nr_blocks, blocks = pixy.get_blocks(5, 1)
+        nr_blocks, blocks = pixy.get_blocks(6, 1)
         pixy.set_lamp(0, 0)
         # Extract data of first (and only) block
         if nr_blocks >= 1:
             colorSIG = blocks[0].sig
             # colorSIZE = blocks[0].width
+            print("Signature of Color", colorSIG)
             if colorSIG == 2:
                 return "purple"
             elif colorSIG == 3:
@@ -245,12 +116,12 @@ def driveToPlace(whichColor):
             robot.stop()
             robot.straight(-150)
             robot.turn(90)
-            robot.straight(-125)
+            robot.straight(-100)
             robot.stop()
             DriveTillDouble(7, -500)
             bremsen()
             motor_left.run_angle(-500, -300)
-            robot.straight(-35)
+            robot.straight(-55)
         
             front_grabber_bottom.run_angle(125, -225)
             front_grabber_top.run_angle(-125, 50)
@@ -258,10 +129,12 @@ def driveToPlace(whichColor):
             front_grabber_top.run_angle(125, 50)
             front_grabber_bottom.run_angle(125, 225)
             robot.turn(35)
-            robot.straight(100)
+            fahren(500)
+            time.sleep(3.5)
             robot.turn(90)
             robot.straight(-300)
             robot.turn(90)
+            robot.stop()
             return
         
         # Grün , linke Seite + rechts
@@ -273,12 +146,12 @@ def driveToPlace(whichColor):
             robot.stop()
             robot.straight(-150)
             robot.turn(-90)
-            robot.straight(-125)
+            robot.straight(-100)
             robot.stop()
             DriveTillDouble(7, -500)
             bremsen()
             motor_left.run_angle(-500, -300)
-            robot.straight(-35)
+            robot.straight(-55)
 
             front_grabber_bottom.run_angle(125, -225)
             front_grabber_top.run_angle(-125, 50)
@@ -286,10 +159,12 @@ def driveToPlace(whichColor):
             front_grabber_top.run_angle(125, 50)
             front_grabber_bottom.run_angle(125, 225)
             robot.turn(35)
-            robot.straight(100)
+            fahren(500)
+            time.sleep(3.5)
             robot.turn(-90)
             robot.straight(-300)
             robot.turn(-90)
+            robot.stop()
             return
         
         # Orange, rechts Seite + rechts
@@ -301,12 +176,12 @@ def driveToPlace(whichColor):
             robot.stop()
             robot.straight(-150)
             robot.turn(90)
-            robot.straight(-125)
+            robot.straight(-100)
             robot.stop()
             DriveTillDouble(7, -500)
             bremsen()
             motor_right.run_angle(-500, -300)
-            robot.straight(35)
+            robot.straight(55)
 
             front_grabber_bottom.run_angle(125, -225)
             front_grabber_top.run_angle(-125, 50)
@@ -314,45 +189,47 @@ def driveToPlace(whichColor):
             front_grabber_top.run_angle(125, 50)
             front_grabber_bottom.run_angle(125, 225)
             robot.turn(-35)
-            robot.straight(100)
+            fahren(500)
+            time.sleep(3.5)
             robot.turn(90)
             robot.straight(-300)
             robot.turn(90)
+            robot.stop()
             return
         
-        # Nichts, rechts Seite + rechts
+        # Nichts
         else:
-            robot.straight(-10)
-            robot.turn(90)
-            isMiddle = KisteErkennen()
+            while True:
+                print("Andere male")
+                robot.straight(-10)
+                robot.turn(90)
 
-            if isMiddle == True:
-                robot.stop()
-                front_grabber_top.run_angle(-300, 65)
+                isMiddle = KisteErkennen()
 
-                robot.straight(-185)
-                robot.straight(25)
-                front_grabber_bottom.run_time(-550, 1400)
-                #front_grabber_bottom.run_angle(200, 5, then=Stop.HOLD)
-                front_grabber_top.run_time(550, 525)
-                front_grabber_bottom.run_time(125, 1800, then=Stop.COAST)
+                if isMiddle == True:
+                    robot.stop()
+                    front_grabber_top.run_angle(-300, 65)
 
-                ## Tütchen analysieren ##
-                robot.straight(200)
-                robot.turn(-90)
-                front_grabber_bottom.run_angle(125, -200)
+                    robot.straight(-185)
+                    robot.straight(25)
+                    front_grabber_bottom.run_time(-550, 1400)
+                    #front_grabber_bottom.run_angle(200, 5, then=Stop.HOLD)
+                    front_grabber_top.run_time(550, 525)
+                    front_grabber_bottom.run_time(125, 1800, then=Stop.COAST)
 
-                detectedColor = BonbonErkennen()
-                print("Erkannte Farbe:", detectedColor)
-                ev3.speaker.beep()
-                front_grabber_bottom.run_time(125, 1800, then=Stop.COAST)
-                front_grabber_bottom.stop()
-                robot.stop()
-                robot.turn(-35)
-                robot.straight(-300)
-                robot.turn(-90)
-                if detectedColor != None:
-                    return
+                    ## Tütchen analysieren ##
+                    robot.straight(200)
+                    robot.turn(-90)
+                    front_grabber_bottom.run_angle(125, -200)
+
+                    detectedColor = BonbonErkennen()
+                    print("Erkannte Farbe:", detectedColor)
+                    ev3.speaker.beep()
+                    front_grabber_bottom.run_time(125, 1800, then=Stop.COAST)
+                    front_grabber_bottom.stop()
+                    robot.stop()
+                    if detectedColor != None:
+                        return
             
 def checkUp():
     robot.straight(-100)
@@ -368,11 +245,11 @@ def checkUp():
     robot.turn(90)
     wait(250)
     robot.straight(90)
-    wait(10000)
+    time.sleep(100)
 
 ##---------------------------- Fahrprogramm ----------------------------##
 ## Anfahren ##
-# checkUp()
+#checkUp()
 robot.straight(-200)
 robot.stop()
 DriveTillColor("right", 7, -300)
@@ -382,7 +259,7 @@ robot.turn(90)
 robot.stop()
 DriveTillDouble(7, 400)
 bremsen()
-robot.straight(75)
+robot.straight(80)
 wait(100)
 robot.turn(91)
 robot.straight(575)
@@ -401,6 +278,7 @@ robot.turn(180)
 
 while True:
     ## In die Kiste grabben ##
+    print("Erstes mal erkennen")
     isMiddle = KisteErkennen()
 
     if isMiddle == True:
